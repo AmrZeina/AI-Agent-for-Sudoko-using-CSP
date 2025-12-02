40f1fa4ac558a0f37ff7022982d3155172c596a4
salmaa203 <154463078+salmaa203@users.noreply.github.com>
2025-12-02 13:54:33 +0200

    Environment.py

--- Environment.py
+++ Environment.py
@@ -11,9 +11,22 @@ class sudoku ():
         self.board = [[0 for _ in range(N)] for _ in range(N)]
 
     #Testing purposes
+    # def printBoard(self):
+    #     for row in self.board:
+    #         print(*row)
+    
     def printBoard(self):
-        for row in self.board:
-            print(*row)
+        for r in range(9):
+           if r % 3 == 0 and r != 0:
+            print("-" * 21)   
+
+           row_str = ""
+           for c in range(9):
+              if c % 3 == 0 and c != 0:
+                row_str += "| "  
+
+              row_str += str(self.board[r][c]) + " "
+           print(row_str)
 
     def isFilled(self):
         for r in range (N):
